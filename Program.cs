a0749ed2a736cec9feb20edd587e126ed7cfbf28
Mphikeleli Mbongiseni Mathonsi <mphikelelimm@gmail.com>
2023-12-04 11:14:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Implement_Role_Based_Authorization.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Implement_Role_Based_AuthorizationContextConnection") ?? throw new InvalidOperationException("Connection string 'Implement_Role_Based_AuthorizationContextConnection' not found.");
+
+builder.Services.AddDbContext<Implement_Role_Based_AuthorizationContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Implement_Role_Based_AuthorizationContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
